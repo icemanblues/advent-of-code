b7239ecd1e357c97616362d53db8ad784aef41bd
Roland Kluge <rkluge@paperlesspost.com>
2018-12-04 14:25:36 -0500

    adding test file. Starting the enrichment of the events

--- golang/src/aoc2018/day04/day4.go
+++ golang/src/aoc2018/day04/day4.go
@@ -22,7 +22,7 @@ type TimeInfo struct {
 	event string
 }
 
-type TimeInfoSlice []TimeInfo
+type TimeInfoSlice []*TimeInfo
 
 func (tis TimeInfoSlice) Len() int {
 	return len(tis)
@@ -34,25 +34,37 @@ func (tis TimeInfoSlice) Less(i, j int) bool {
 	return tis[i].Time.Before(tis[j].Time)
 }
 
+var EventState = struct {
+	Asleep string
+	Awake  string
+	Begin  string
+}{
+	Asleep: "falls asleep",
+	Awake:  "wakes up",
+	Begin:  "begins shift",
+}
+
 func part1() {
 	fmt.Println("Day 04 Part 1")
 
-	file, _ := os.Open("input04.txt")
+	file, _ := os.Open("test.txt")
 	defer file.Close()
 
-	const timeFormat = "2006-01-04 15:05"
+	// year-month-day hour:minute
+	// Mon Jan 2 15:04:05 -0700 MST 2006
+	// RFC3339     =   "2006-01-02T15:04:05Z07:00"
+	const timeFormat = "2006-01-02 15:04"
 
 	var events TimeInfoSlice
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		runes := []rune(line)
 		// [1518-11-01 00:00] Guard #10 begins shift
 		// [1518-11-01 00:05] falls asleep
 		// [1518-11-01 00:25] wakes up
-		lineTime := string(runes[1:17])
-		lineInfo := string(runes[19:])
+		lineTime := line[1:17]
+		lineInfo := line[19:]
 
 		t, err := time.Parse(timeFormat, lineTime)
 		if err != nil {
@@ -60,16 +72,17 @@ func part1() {
 			break
 		}
 
-		info := TimeInfo{
+		info := &TimeInfo{
 			Time:  t,
 			event: lineInfo,
 		}
-		if lineInfo != "falls asleep" && lineInfo != "wakes up" {
+		if lineInfo != EventState.Asleep && lineInfo != EventState.Awake {
 			infoParts := strings.Split(lineInfo, " ")
 			idPart := infoParts[1][1:]
 			info.ID, err = strconv.Atoi(idPart)
 			if err != nil {
 				fmt.Printf("Bad time when parsing Guard ID: %v\n", idPart)
+				break
 			}
 		}
 
@@ -79,7 +92,19 @@ func part1() {
 	sort.Sort(events)
 
 	// now need to copy the guard id to all of its awake and asleep events
-	for _, e := range events[:15] {
+	var guard int
+	for _, e := range events {
+		if e.event == EventState.Asleep {
+			e.ID = guard
+		} else if e.event == EventState.Awake {
+			e.ID = guard
+		} else {
+			// new guard is begining
+			guard = e.ID
+		}
+	}
+
+	for _, e := range events {
 		fmt.Println(e)
 	}
 }
