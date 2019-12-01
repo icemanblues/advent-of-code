21aeb7008c4ee823ce58a79fc7db071b3a796691
Roland Kluge <rolandkluge@yahoo.com>
2019-12-01 16:07:25 -0500

    formatting to match the ts template

--- 2019/day01/day01.ts
+++ 2019/day01/day01.ts
@@ -1,14 +1,7 @@
-//
-//
-//
-
 import fs from 'fs';
-import readline from 'readline';
 
 const dayNum: string = "01";
 const dayTitle: string = "The Tyranny of the Rocket Equation";
-const subTitle1: string = "subtitle 1";
-const subTitle2: string = "subtitle 2";
 
 // reads entire file into memory, then splits it
 function readInputSync(filename: string): string[] {
@@ -17,30 +10,15 @@ function readInputSync(filename: string): string[] {
     return lines;
 }
 
-async function readInput(filename: string): Promise<string[]> {
-    const lines: string[] = [];
-    const fileStream: fs.ReadStream = fs.createReadStream(filename);
-    const rl: readline.Interface = readline.createInterface({
-        input: fileStream,
-        crlfDelay: Infinity,
-    });12
-
-    rl.on('line', (line: string) => lines.push(line));
-    return new Promise<string[]>((resolve) => {
-        rl.on('close', () => resolve(lines));
-    });
-}
-
 function fuelReq(mass: number): number {
     return Math.floor(mass / 3) - 2;
 }
 
 function fuelFuel(mass: number): number {
     let fuel: number = 0;
-    while(mass > 0) {
+    while (mass > 0) {
         let f: number = fuelReq(mass);
-        //console.log(`mass ${mass} with fuel ${f}`);
-        if( f > 0 ) {
+        if (f > 0) {
             fuel += f;
             mass = f;
         } else {
@@ -50,35 +28,35 @@ function fuelFuel(mass: number): number {
     return fuel;
 }
 
-async function part1() {
-    console.log(`Part 1: ${subTitle1}`);
+function part1() {
+    console.log('Part 1');
     console.log(fuelReq(12));
     console.log(fuelReq(14));
     console.log(fuelReq(1969));
     console.log(fuelReq(100756));
 
-    const inputs: string[] = await readInput('input.txt');
-    const p1: number = inputs.reduce( (acc, curr) => acc+fuelReq(Number(curr)), 0);
-    console.log(`part1 ${p1}`);   
+    const inputs: string[] = readInputSync('input.txt');
+    const p1: number = inputs.reduce((acc, curr) => acc + fuelReq(Number(curr)), 0);
+    console.log(`part1 ${p1}`);
 }
 
-async function part2() {
-    console.log(`Part 2: ${subTitle2}`);
+function part2() {
+    console.log('Part 2');
     console.log(fuelFuel(12));
     console.log(fuelFuel(14));
     console.log(fuelFuel(1969));
     console.log(fuelFuel(100756));
 
-    const inputs: string[] = await readInput('input.txt');
-    const p2: number = inputs.reduce( (acc, curr) => acc+fuelFuel(Number(curr)), 0);
-    console.log(`part2 ${p2}`);   
+    const inputs: string[] = readInputSync('input.txt');
+    const p2: number = inputs.reduce((acc, curr) => acc + fuelFuel(Number(curr)), 0);
+    console.log(`part2 ${p2}`);
 }
 
-async function main() {
+function main() {
     console.log(`Day ${dayNum} : ${dayTitle}`);
 
-    await part1();
-    await part2();
+    part1();
+    part2();
 }
 
 main();
