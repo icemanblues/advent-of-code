1fa1171a6a1c0c03ff9ef399005e3fd95b1737d6
Roland Kluge <rolandkluge@yahoo.com>
2019-12-06 01:29:04 -0500

    2019 day06 completed

--- 2019/day06/day06.ts
+++ 2019/day06/day06.ts
@@ -1,27 +1,111 @@
 import fs from 'fs';
 
-const dayNum: string = "XX";
-const dayTitle: string = "Title";
+const dayNum: string = "06";
+const dayTitle: string = "Universal Orbit Map";
 
-// reads entire file into memory, then splits it
 function readInputSync(filename: string): string[] {
     const contents: string = fs.readFileSync(filename, "utf-8");
     const lines: string[] = contents.trimRight().split(/\r?\n/);
     return lines;
 }
 
+const lines: string[] = readInputSync('input.txt');
+
+function orbits(lines: string[]): Map<string, string> {
+    const m: Map<string, string> = new Map<string, string>();
+
+    lines.forEach(l => {
+        const planets: string[] = l.split(')');
+        m.set(planets[1], planets[0]);
+    });
+
+    return m;
+}
+
 function part1() {
     console.log('Part 1');
 
-    const lines: string[] = readInputSync('input.txt');
-    console.log(lines);
+    const orbitMap = orbits(lines);
+
+    const s: Set<string> = new Set<string>();
+    orbitMap.forEach((v, k) => {
+        s.add(k);
+        s.add(v);
+    });
+
+    let count: number = 0;
+    s.forEach(k => {
+        let curr: string = k;
+        let currbit: string = orbitMap.get(k);
+        while (currbit) {
+            count++;
+            curr = currbit;
+            currbit = orbitMap.get(curr);
+        }
+    });
+    console.log(count);
+}
+
+class Move {
+    planet: string;
+    count: number;
+
+    constructor(p: string, c: number) {
+        this.planet = p;
+        this.count = c;
+    }
+}
+
+function edges(lines: string[]): Map<string, string[]> {
+    const e: Map<string, string[]> = new Map<string, string[]>();
+
+    lines.forEach(l => {
+        const planets: string[] = l.split(')');
+        let list: string[] = e.get(planets[0]);
+        if (!list) {
+            list = [];
+        }
+        list.push(planets[1]);
+        e.set(planets[0], list);
+
+        list = e.get(planets[1]);
+        if (!list) {
+            list = [];
+        }
+        list.push(planets[0]);
+        e.set(planets[1], list);
+    });
+
+    return e;
 }
 
 function part2() {
     console.log('Part 2');
 
-    const lines: string[] = readInputSync('input.txt');
-    console.log(lines);
+    const e: Map<string, string[]> = edges(lines);
+    const visited: Set<string> = new Set<string>();
+    const start: Move = new Move('YOU', 0);
+    const queue: Move[] = [start];
+
+    while (queue.length !== 0) {
+        const curr: Move = queue.shift();
+
+        if (curr.planet === 'SAN') {
+            console.log(curr.count - 2);
+            break;
+        }
+
+        const children: string[] = e.get(curr.planet);
+        if (children) {
+            children.forEach(c => {
+                if (!visited.has(c)) {
+                    const m: Move = new Move(c, curr.count + 1);
+                    queue.push(m);
+                    visited.add(c);
+                }
+            });
+        }
+    }
 }
 
 function main() {
