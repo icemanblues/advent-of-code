44264ca99d7b0ba1254aa8c31b1f59857641ba7c
Roland Kluge <rolandkluge@yahoo.com>
2019-12-17 01:00:10 -0500

    adding optional offset

--- 2019/day16/day16.ts
+++ 2019/day16/day16.ts
@@ -12,37 +12,34 @@ function readInputSync(filename: string): number[] {
 
 const BASE: number[] = [0, 1, 0, -1];
 
-function dotProduct(input: number[], pattern: number[]): number {
+function dotProduct(input: number[], pattern: number[], offset: number = 0): number {
     let sum: number = 0;
     for (let i = 0; i < input.length; i++) {
-        sum += input[i] * pattern[i];
+        sum += input[i] * pattern[(i + 1 + offset) % pattern.length];
     }
     return Math.abs(sum) % 10;
 };
 
-function computePattern(base: number[], position: number, len: number): number[] {
+function computePattern(base: number[], position: number): number[] {
     const r: number[] = [];
 
-    let i = 0;
-    while (r.length <= len) {
-        for (let j = 0; j <= position; j++) {
-            r.push(base[i % base.length]);
+
+    for (let b of BASE) {
+        for (let i = 0; i <= position; i++) {
+            r.push(b);
         }
-        i++;
     }
 
-
-    r.shift();
     return r;
 }
 
-function fft(input: number[], phases: number): string {
+function fft(input: number[], phases: number, offset: number = 0): string {
     let result: number[] = input;
     for (let i = 1; i <= phases; i++) {
         let temp: number[] = [];
         for (let j = 0; j < result.length; j++) {
-            const pattern = computePattern(BASE, j, result.length);
-            const dp = dotProduct(result, pattern);
+            const pattern = computePattern(BASE, j);
+            const dp = dotProduct(result, pattern, offset);
             temp.push(dp); // temp[j] =
         }
         result = temp;
@@ -62,13 +59,23 @@ function part1() {
 
 function part2() {
     console.log('Part 2');
+    const input = readInputSync('input.txt');
+    const offset = Number(input.slice(0, 7).join(''));
+
+    let big: number[] = [];
+    for (let i = 0; i < 10000; i++) {
+        big = big.concat(input);
+    }
+    big = big.slice(offset);
+    const output = fft(big, 100)
+    console.log('Part 1', output.substring(0, 8));
 }
 
 function main() {
     console.log(`Day ${dayNum} : ${dayTitle}`);
 
     part1();
-    part2();
+    //part2();
 }
 
 main();
