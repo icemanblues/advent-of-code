6f42e23606746a82d932086afd2c09b7c7977825
Roland Kluge <rolandkluge@yahoo.com>
2019-12-17 21:28:54 -0500

    2019/day16 completed

--- 2019/day16/day16.ts
+++ 2019/day16/day16.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import * as fs from 'fs';
 
 const dayNum: string = "16";
 const dayTitle: string = "Flawed Frequency Transmission";
@@ -12,25 +12,22 @@ function readInputSync(filename: string): number[] {
 
 const BASE: number[] = [0, 1, 0, -1];
 
+function computePattern(base: number[], position: number): number[] {
+    const r: number[] = [];
+    for (let b of BASE) {
+        for (let i = 0; i <= position; i++) {
+            r.push(b);
+        }
+    }
+    return r;
+}
+
 function dotProduct(input: number[], pattern: number[], offset: number = 0): number {
     let sum: number = 0;
     for (let i = 0; i < input.length; i++) {
         sum += input[i] * pattern[(i + 1 + offset) % pattern.length];
     }
     return Math.abs(sum) % 10;
-};
-
-function computePattern(base: number[], position: number): number[] {
-    const r: number[] = [];
-
-
-    for (let b of BASE) {
-        for (let i = 0; i <= position; i++) {
-            r.push(b);
-        }
-    }
-
-    return r;
 }
 
 function fft(input: number[], phases: number, offset: number = 0): string {
@@ -45,7 +42,22 @@ function fft(input: number[], phases: number, offset: number = 0): string {
         result = temp;
     }
 
-    //return result;
+    return result.join('');
+}
+
+function dpOptimized(input: number[]): void {
+    let sum = 0;
+    for (let i = input.length - 1; i >= 0; i--) {
+        sum += input[i];
+        input[i] = Math.abs(sum) % 10;
+    }
+}
+
+function fftOptimized(input: number[], phases: number): string {
+    let result: number[] = input;
+    for (let p = 1; p <= phases; p++) {
+        dpOptimized(result);
+    }
     return result.join('');
 }
 
@@ -53,7 +65,6 @@ function part1() {
     console.log('Part 1');
     const input = readInputSync('input.txt');
     const output = fft(input, 100)
-
     console.log('Part 1', output.substring(0, 8));
 }
 
@@ -64,18 +75,21 @@ function part2() {
 
     let big: number[] = [];
     for (let i = 0; i < 10000; i++) {
-        big = big.concat(input);
+        for (let e of input) {
+            big.push(e);
+        }
     }
     big = big.slice(offset);
-    const output = fft(big, 100)
+
+    const output = fftOptimized(big, 100)
     console.log('Part 1', output.substring(0, 8));
 }
 
 function main() {
     console.log(`Day ${dayNum} : ${dayTitle}`);
 
-    part1();
-    //part2();
+    part1(); // 82435530
+    part2(); // 83036156
 }
 
 main();
