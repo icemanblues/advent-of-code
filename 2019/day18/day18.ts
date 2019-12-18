45578af58f53bfeb6ae3f5aa8f96d489d86df966
Roland Kluge <rolandkluge@yahoo.com>
2019-12-18 00:12:07 -0500

    2019/day18 go

--- 2019/day18/day18.ts
+++ 2019/day18/day18.ts
@@ -1,32 +1,44 @@
 import fs from 'fs';
 
-const dayNum: string = "XX";
-const dayTitle: string = "Title";
+const dayNum: string = "18";
+const dayTitle: string = "Many-Worlds Interpretation";
 
-// reads entire file into memory, then splits it
-function readInputSync(filename: string): string[] {
-    const contents: string = fs.readFileSync(filename, "utf-8");
-    const lines: string[] = contents.trimRight().split(/\r?\n/);
-    return lines;
+function read(filename: string): string[] {
+    return fs.readFileSync(filename, "utf-8").trimRight().split(/\r?\n/);
 }
+
+const input = read('input.txt');
+
+function print(board: string[]): void {
+    for (let l of board) {
+        console.log(l);
+    }
+}
+
+function find(board: string[], s: string): [number, number] {
+    for (let y = 0; y < board.length; y++) {
+        const x = board[y].indexOf(s);
+        if (x !== -1) {
+            return [x, y];
+        }
+    }
+
+    return [-1, -1];
+}
+
 
 function part1() {
     console.log('Part 1');
-
-    const lines: string[] = readInputSync('input.txt');
-    console.log(lines);
+    print(input);
+    const start = find(input, '@');
 }
 
 function part2() {
     console.log('Part 2');
-
-    const lines: string[] = readInputSync('input.txt');
-    console.log(lines);
 }
 
 function main() {
     console.log(`Day ${dayNum} : ${dayTitle}`);
-
     part1();
     part2();
 }
