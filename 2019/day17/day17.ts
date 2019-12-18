0b7f533851bee961b162a5f34792a1aaf5ff6924
Roland Kluge <rolandkluge@yahoo.com>
2019-12-17 19:45:01 -0500

    2019/day17 completed

--- 2019/day17/day17.ts
+++ 2019/day17/day17.ts
@@ -6,12 +6,6 @@ import { strt, str, tuple } from '../util';
 const dayNum: string = "17";
 const dayTitle: string = "Set and Forget";
 
-const ascii: Map<number, string> = new Map([
-    [35, '#'],
-    [46, '.'],
-    [10, '\n']
-]);
-
 function intersection(grid: string[][]): number {
     let sum = 0;
     for (let y = 0; y < grid.length; y++) {
@@ -41,32 +35,18 @@ function intersection(grid: string[][]): number {
 }
 
 function buildMap(arr: number[]): string[][] {
-    let x = 0;
-    let y = 0;
-
     const map: string[][] = [];
     let row: string[] = [];
     while (arr.length !== 0) {
         const n = arr.shift();
-        const tile = ascii.get(n);
+        const tile = String.fromCharCode(n);
         switch (tile) {
-            case '#':
-                row.push(tile);
-                x++;
-                break;
-            case '.':
-                row.push(tile);
-                x++;
-                break;
             case '\n':
                 map.push(row);
-                y++;
-                x = 0;
                 row = [];
                 break;
             default:
-                row.push(String.fromCharCode(n));
-                x++;
+                row.push(tile);
                 break;
         }
     }
@@ -77,32 +57,6 @@ function buildMap(arr: number[]): string[][] {
 function displayGrid(grid: string[][]): void {
     for (let y = 0; y < grid.length; y++) {
         console.log(grid[y].join(''));
-    }
-}
-
-const MEM_LIMIT = 20;
-
-const grammar = new Map<string, number>([
-    [',', 44],
-    ['\n', 10]
-]);
-
-const COMMA = 44;
-const routines = new Map<string, number>([
-    ['A', 65],
-    ['B', 66],
-    ['C', 66]
-]);
-
-const movement = new Map<string, number>([
-    ['L', 76],
-    ['R', 82]
-]);
-
-
-function go(amp: Amp) {
-    while (!amp.isHalted) {
-        progAmp(amp);
     }
 }
 
@@ -117,31 +71,35 @@ function part1() {
     console.log(intersection(grid));
 }
 
+function toAscii(command: string): number[] {
+    const a = command.split('').map(s => s.charCodeAt(0));
+    a.push(10); // new line
+    return a;
+}
+
 function part2() {
     console.log('Part 2');
     const intcode = parseIntcode('input.txt');
     intcode[0] = 2;
 
+    const main = toAscii('A,B,A,C,A,B,C,B,C,B');
+    const amove = toAscii('L,10,R,8,L,6,R,6');
+    const bmove = toAscii('L,8,L,8,R,8');
+    const cmove = toAscii('R,8,L,6,L,10,L,10');
+    const video = toAscii('n');
 
-    const main = [65, 44, 66, 44, 67, 44, 66, 44, 65, 44, 67, 10];
-    const amove = [82, 44, 56, 44, 82, 44, 56, 10];
-    const bmove = [82, 44, 52, 44, 82, 44, 52, 44, 82, 44, 56, 10];
-    const cmove = [76, 44, 54, 44, 76, 44, 50, 10];
-    const video = [89, 10]; // y
-    //const video = [78, 10]; // no
     const input: number[] = main.concat(amove, bmove, cmove, video);
     const output: number[] = [];
     const amp = new Amp('Aft', intcode, input, output);
     prog(amp);
 
-    const grid = buildMap(output);
-    displayGrid(grid);
+    console.log(output[output.length - 1]);
 }
 
 function main() {
     console.log(`Day ${dayNum} : ${dayTitle}`);
 
-    //part1();
+    part1();
     part2();
 }
 
