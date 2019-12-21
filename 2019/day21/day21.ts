eb498596f9a4b1e7771f5883666a63fadd414a65
Roland Kluge <rolandkluge@yahoo.com>
2019-12-21 15:31:38 -0500

    2019/day21 part1

--- 2019/day21/day21.ts
+++ 2019/day21/day21.ts
@@ -1,15 +1,62 @@
 import * as fs from 'fs';
+import { Amp, progAmp, prog } from '../intcode';
 
-const dayNum: string = "XX";
-const dayTitle: string = "Title";
+const dayNum: string = "21";
+const dayTitle: string = "Springdroid Adventure";
 
-function readInputSync(filename: string): string[] {
+function read(filename: string): number[] {
     return fs.readFileSync(filename, "utf-8")
-        .trimRight().split(/\r?\n/);
+        .trimRight().split(/,/).map(Number);
+}
+
+function readSpringscript(filename: string): string[] {
+    return fs.readFileSync(filename, "utf-8")
+        .trimRight().split(/\n/);
+}
+
+const ASCII_LENGTH = 128; //0-127
+const MAX_LENGTH = 15;
+
+function toAscii(springscript: string[]): number[] {
+    const r: number[] = [];
+    springscript.forEach(s => {
+        for (let c of s) {
+            r.push(c.charCodeAt(0));
+        }
+        r.push(10); // new line
+    });
+    return r;
+}
+
+function display(output: number[]): number {
+    let score: number = -1;
+    let render: string[] = [];
+    while (output.length !== 0) {
+        const o = output.shift();
+        if (o >= ASCII_LENGTH) {
+            score = o;
+        } else {
+            render.push(String.fromCharCode(o));
+        }
+    }
+
+    console.log(render.join(''));
+    return score;
+}
+
+function jump(file: string) {
+    const springscript: string[] = readSpringscript(file);
+    const intcode = read('input.txt');
+    const output: number[] = [];
+    const input: number[] = toAscii(springscript);
+    const amp = new Amp('Day 21', intcode, input, output);
+    prog(amp);
+    console.log(display(output));
 }
 
 function part1() {
     console.log('Part 1');
+    jump('part1.txt');
 }
 
 function part2() {
