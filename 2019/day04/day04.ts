3d45073640449089cd99e4ff42cb9ab6aea52680
Roland Kluge <roland.kluge@gmail.com>
2019-12-22 22:56:04 -0500

    2019/day04 completed

--- 2019/day04/day04.ts
+++ 2019/day04/day04.ts
@@ -1,32 +1,92 @@
-import fs from 'fs';
+const dayNum: string = "04";
+const dayTitle: string = "Secure Container";
 
-const dayNum: string = "XX";
-const dayTitle: string = "Title";
+const [INPUT_MIN, INPUT_MAX] = [197487, 673251];
 
-// reads entire file into memory, then splits it
-function readInputSync(filename: string): string[] {
-    const contents: string = fs.readFileSync(filename, "utf-8");
-    const lines: string[] = contents.trimRight().split(/\r?\n/);
-    return lines;
+function sixDigit(s: string): boolean {
+    return s.length === 6;
+}
+
+function withinRange(s: string): boolean {
+    const n = Number(s);
+    return n >= INPUT_MIN && n <= INPUT_MAX;
+}
+
+function adjacentSame(s: string): boolean {
+    for (let i = 0; i < s.length - 1; i++) {
+        if (s.charAt(i) === s.charAt(i + 1)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function neverDecrease(s: string): boolean {
+    let n = Number(s.charAt(0));
+    for (let i = 1; i < s.length; i++) {
+        const j = Number(s.charAt(i));
+        if (j < n) {
+            return false;
+        }
+        n = j;
+    }
+
+    return true;
+}
+
+function isValid(s: string): boolean {
+    return sixDigit(s) && withinRange(s) && adjacentSame(s) && neverDecrease(s);
+}
+
+function password(valid: (s: string) => boolean): number {
+    let count = 0;
+    for (let i = INPUT_MIN; i <= INPUT_MAX; i++) {
+        const s = String(i);
+        if (valid(s)) {
+            count++;
+        }
+    }
+    return count;
 }
 
 function part1() {
     console.log('Part 1');
+    console.log(password(isValid));
+}
 
-    const lines: string[] = readInputSync('input.txt');
-    console.log(lines);
+function adjOnlyTwo(s: string): boolean {
+    let c = s.charAt(0);
+    let count = 1;
+
+    let i = 1;
+    while (i < s.length) {
+        if (c === s.charAt(i)) {
+            count++;
+        } else {
+            if (count === 2) {
+                return true;
+            }
+            count = 1;
+            c = s.charAt(i);
+        }
+
+        i++;
+    }
+
+    return count === 2;
+}
+
+function isValidTwo(s: string): boolean {
+    return sixDigit(s) && withinRange(s) && adjOnlyTwo(s) && neverDecrease(s);
 }
 
 function part2() {
     console.log('Part 2');
-
-    const lines: string[] = readInputSync('input.txt');
-    console.log(lines);
+    console.log(password(isValidTwo));
 }
 
 function main() {
     console.log(`Day ${dayNum} : ${dayTitle}`);
-
     part1();
     part2();
 }
