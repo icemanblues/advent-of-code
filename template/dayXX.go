6cfe3d074ec755974617c0e092067b89f00ff537
Roland Kluge <roland.kluge@gmail.com>
2020-11-29 19:12:46 -0500

    simplying
    Moving the read input function to a util package. it is not always needed

--- template/dayXX.go
+++ template/dayXX.go
@@ -1,32 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 const (
 	dayNum   = "XX"
 	dayTitle = "Title"
 )
-
-func readInput(filename string) []string {
-	file, err := os.Open(filename)
-	if err != nil {
-		fmt.Printf("something bad with file: %v\n", err)
-		return nil
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	return lines
-}
 
 func part1() {
 	fmt.Println("Part 1")
