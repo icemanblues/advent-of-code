6655e53648cea27c289df466623a77b500e7c248
Roland Kluge <roland.kluge@gmail.com>
2020-12-01 00:18:36 -0500

    day01 complete

--- 2020/day01/day01.go
+++ 2020/day01/day01.go
@@ -7,21 +7,54 @@ import (
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "01"
+	dayTitle = "Report Repair"
 )
 
 func part1() {
 	fmt.Println("Part 1")
-	inputs, err := util.ReadInput("input1.txt")
+	inputs, err := util.ReadIntput("input1.txt")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(inputs)
+
+	nums := make(map[int]struct{})
+	for _, n := range inputs {
+		nums[n] = struct{}{}
+
+		m := 2020 - n
+		if _, ok := nums[m]; ok {
+			mult := n * m
+			fmt.Println(mult)
+			break
+		}
+	}
+
 }
 
 func part2() {
 	fmt.Println("Part 2")
+	inputs, _ := util.ReadIntput("input1.txt")
+	nums := make(map[int]struct{})
+
+outer:
+	for i, n := range inputs {
+		nums[n] = struct{}{}
+
+		t := 2020 - n
+		for j, nn := range inputs {
+			if i == j {
+				continue
+			}
+
+			m := t - nn
+			if _, ok := nums[m]; ok {
+				mult := n * m * nn
+				fmt.Println(mult)
+				break outer
+			}
+		}
+	}
 }
 
 func main() {
