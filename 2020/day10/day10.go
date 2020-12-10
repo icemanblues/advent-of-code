23f8b9391e0db70c70cc00f384b28add1e33e9fa
Roland Kluge <roland.kluge@gmail.com>
2020-12-10 00:39:21 -0500

    day 10 complete

--- 2020/day10/day10.go
+++ 2020/day10/day10.go
@@ -2,19 +2,49 @@ package main
 
 import (
 	"fmt"
+	"sort"
+
+	"github.com/icemanblues/advent-of-code/2020/util"
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "10"
+	dayTitle = "Adapter Array"
 )
 
 func part1() {
-	fmt.Println("Part 1")
+	curr := 0
+	ints, _ := util.ReadIntput("input.txt")
+	sort.Ints(ints)
+
+	diff1 := 0
+	diff3 := 1
+	for _, jolt := range ints {
+		if jolt-curr == 1 {
+			diff1++
+			curr = jolt
+		}
+		if jolt-curr == 3 {
+			diff3++
+			curr = jolt
+		}
+	}
+
+	fmt.Printf("Part 1, %v\n", diff1*diff3)
 }
 
 func part2() {
-	fmt.Println("Part 2")
+	ints, _ := util.ReadIntput("input.txt")
+	sort.Ints(ints)
+	paths := make(map[int]int)
+	paths[0] = 1
+
+	for _, jolt := range ints {
+		paths[jolt] = paths[jolt-1] + paths[jolt-2] + paths[jolt-3]
+	}
+
+	fmt.Println(paths)
+	fmt.Printf("Part 2: %v\n", paths[ints[len(ints)-1]])
 }
 
 func main() {
