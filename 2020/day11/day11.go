af7def275709be769f87aa39a8cded9e599b0370
Roland Kluge <roland.kluge@gmail.com>
2020-12-11 01:05:37 -0500

    day11 cleaned up

--- 2020/day11/day11.go
+++ 2020/day11/day11.go
@@ -18,19 +18,19 @@ func seatCount(seats [][]rune) int {
 	return count
 }
 
-func seatOfLife(seatMap [][]rune) [][]rune {
-	seatMapper := make([][]rune, len(seatMap), len(seatMap))
-	for i := range seatMapper {
-		seatMapper[i] = make([]rune, len(seatMap[i]), len(seatMap[i]))
+func seatAdj(seatMap [][]rune) [][]rune {
+	nextMap := make([][]rune, len(seatMap), len(seatMap))
+	for i := range nextMap {
+		nextMap[i] = make([]rune, len(seatMap[i]), len(seatMap[i]))
 	}
 
 	for i, line := range seatMap {
 		for j, s := range line {
 			if s == '.' {
-				seatMapper[i][j] = '.'
+				nextMap[i][j] = '.'
 				continue
 			}
-
+			// count adjacent taken seats
 			count := 0
 			for ii := -1; ii <= 1; ii++ {
 				for jj := -1; jj <= 1; jj++ {
@@ -44,176 +44,76 @@ func seatOfLife(seatMap [][]rune) [][]rune {
 					}
 				}
 			}
+			// apply seat change logic
 			if s == '#' && count >= 4 {
-				seatMapper[i][j] = 'L'
+				nextMap[i][j] = 'L'
 			} else if s == 'L' && count == 0 {
-				seatMapper[i][j] = '#'
+				nextMap[i][j] = '#'
 			} else {
-				seatMapper[i][j] = s
+				nextMap[i][j] = s
 			}
 		}
 	}
-	return seatMapper
+	return nextMap
 }
 
-func seatOfVisible(seatMap [][]rune) [][]rune {
-	seatMapper := make([][]rune, len(seatMap), len(seatMap))
-	for i := range seatMapper {
-		seatMapper[i] = make([]rune, len(seatMap[i]), len(seatMap[i]))
+func isOccupied(seatMap [][]rune, x, y int, dx, dy int) int {
+	for x+dx >= 0 && x+dx < len(seatMap) && y+dy >= 0 && y+dy < len(seatMap[x]) {
+		if seatMap[x+dx][y+dy] == '#' {
+			return 1
+		}
+		if seatMap[x+dx][y+dy] == 'L' {
+			return 0
+		}
+
+		x += dx
+		y += dy
+	}
+	return 0
+}
+
+func seatLos(seatMap [][]rune) [][]rune {
+	nextMap := make([][]rune, len(seatMap), len(seatMap))
+	for i := range nextMap {
+		nextMap[i] = make([]rune, len(seatMap[i]), len(seatMap[i]))
 	}
 
 	for i, line := range seatMap {
 		for j, s := range line {
 			if s == '.' {
-				seatMapper[i][j] = '.'
+				nextMap[i][j] = '.'
 				continue
 			}
-
+			// count seats in line of sight
 			count := 0
-			// up
-			for k := 1; j-k >= 0; k++ {
-				if seatMap[i][j-k] == '.' {
-					continue
-				}
-				if seatMap[i][j-k] == '#' {
-					count++
-					break
-				}
-				if seatMap[i][j-k] == 'L' {
-					break
-				}
-			}
-
-			// down
-			for k := 1; j+k < len(seatMap[i]); k++ {
-				if seatMap[i][j+k] == '.' {
-					continue
-				}
-				if seatMap[i][j+k] == '#' {
-					count++
-					break
-				}
-				if seatMap[i][j+k] == 'L' {
-					break
-				}
-			}
-
-			// right
-			for k := 1; i+k < len(seatMap); k++ {
-				if seatMap[i+k][j] == '.' {
-					continue
-				}
-				if seatMap[i+k][j] == '#' {
-					count++
-					break
-				}
-				if seatMap[i+k][j] == 'L' {
-					break
-				}
-			}
-
-			// left
-			for k := 1; i-k >= 0; k++ {
-				if seatMap[i-k][j] == '.' {
-					continue
-				}
-				if seatMap[i-k][j] == '#' {
-					count++
-					break
-				}
-				if seatMap[i-k][j] == 'L' {
-					break
-				}
-			}
-
-			// ur
-			for k := 1; i+k < len(seatMap) && j-k >= 0; k++ {
-				if seatMap[i+k][j-k] == '.' {
-					continue
-				}
-				if seatMap[i+k][j-k] == '#' {
-					count++
-					break
-				}
-				if seatMap[i+k][j-k] == 'L' {
-					break
-				}
-			}
-
-			// ul
-			for k := 1; i-k >= 0 && j-k >= 0; k++ {
-				if seatMap[i-k][j-k] == '.' {
-					continue
-				}
-				if seatMap[i-k][j-k] == '#' {
-					count++
-					break
-				}
-				if seatMap[i-k][j-k] == 'L' {
-					break
-				}
-			}
-
-			// dr
-			for k := 1; i+k < len(seatMap) && j+k < len(seatMap[i]); k++ {
-				if seatMap[i+k][j+k] == '.' {
-					continue
-				}
-				if seatMap[i+k][j+k] == '#' {
-					count++
-					break
-				}
-				if seatMap[i+k][j+k] == 'L' {
-					break
-				}
-			}
-
-			// dl
-			for k := 1; i-k >= 0 && j+k < len(seatMap[i]); k++ {
-				if seatMap[i-k][j+k] == '.' {
-					continue
-				}
-				if seatMap[i-k][j+k] == '#' {
-					count++
-					break
-				}
-				if seatMap[i-k][j+k] == 'L' {
-					break
-				}
-			}
-
+			count += isOccupied(seatMap, i, j, 1, 0)
+			count += isOccupied(seatMap, i, j, -1, 0)
+			count += isOccupied(seatMap, i, j, 0, 1)
+			count += isOccupied(seatMap, i, j, 0, -1)
+			count += isOccupied(seatMap, i, j, 1, 1)
+			count += isOccupied(seatMap, i, j, 1, -1)
+			count += isOccupied(seatMap, i, j, -1, 1)
+			count += isOccupied(seatMap, i, j, -1, -1)
+			// apply seat change logic
 			if s == '#' && count >= 5 {
-				seatMapper[i][j] = 'L'
+				nextMap[i][j] = 'L'
 			} else if s == 'L' && count == 0 {
-				seatMapper[i][j] = '#'
+				nextMap[i][j] = '#'
 			} else {
-				seatMapper[i][j] = s
+				nextMap[i][j] = s
 			}
 		}
 	}
-	return seatMapper
+	return nextMap
 }
 
-func seatIter(filename string) int {
+func seatOfLife(filename string, tick func([][]rune) [][]rune) int {
 	seatMap, _ := util.ReadRuneput(filename)
 	prev := -1
 	count := seatCount(seatMap)
 	for count != prev {
 		prev = count
-		seatMap = seatOfLife(seatMap)
-		count = seatCount(seatMap)
-	}
-
-	return count
-}
-
-func seatIterVis(filename string) int {
-	seatMap, _ := util.ReadRuneput(filename)
-	prev := -1
-	count := seatCount(seatMap)
-	for count != prev {
-		prev = count
-		seatMap = seatOfVisible(seatMap)
+		seatMap = tick(seatMap)
 		count = seatCount(seatMap)
 	}
 
@@ -221,11 +121,11 @@ func seatIterVis(filename string) int {
 }
 
 func part1() {
-	fmt.Printf("Part 1: %v\n", seatIter("input.txt"))
+	fmt.Printf("Part 1: %v\n", seatOfLife("input.txt", seatAdj))
 }
 
 func part2() {
-	fmt.Printf("Part 2: %v\n", seatIterVis("input.txt"))
+	fmt.Printf("Part 2: %v\n", seatOfLife("input.txt", seatLos))
 }
 
 func main() {
