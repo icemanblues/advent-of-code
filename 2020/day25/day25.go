d49d6f6bc17d0c8b3f59d852eea3f92c0c3e19cf
Roland Kluge <roland.kluge@gmail.com>
2020-12-25 00:39:30 -0500

    day25 completed and cleaned up

--- 2020/day25/day25.go
+++ 2020/day25/day25.go
@@ -5,20 +5,50 @@ import (
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "25"
+	dayTitle = "Combo Breaker"
 )
 
-func part1() {
-	fmt.Println("Part 1")
+const (
+	card = 18356117
+	door = 5909654
+)
+
+func transform(subject, loopSize int) int {
+	value := 1
+	for i := 0; i < loopSize; i++ {
+		value *= subject
+		value = value % 20201227
+	}
+	return value
 }
 
-func part2() {
-	fmt.Println("Part 2")
+func findLoopSize(target int) int {
+	value := 1
+	i := 0
+	for target != value {
+		value *= 7
+		value = value % 20201227
+		i++
+	}
+	return i
+}
+
+func part1() {
+	cardLoop := findLoopSize(card)
+	doorLoop := findLoopSize(door)
+
+	var answer int
+	if doorLoop < cardLoop {
+		answer = transform(card, doorLoop)
+	} else {
+		answer = transform(door, cardLoop)
+	}
+
+	fmt.Printf("Part 1: %v\n", answer)
 }
 
 func main() {
 	fmt.Printf("Day %v: %v\n", dayNum, dayTitle)
 	part1()
-	part2()
 }
