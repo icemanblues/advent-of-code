b689d3b12249251220d0daa7a6a44316832aef68
Roland Kluge <roland.kluge@gmail.com>
2020-12-27 16:54:04 -0500

    2017 day10 complete

---
+++ 2017/day10/day10.py
@@ -0,0 +1,87 @@
+from typing import List
+
+day_num = "10"
+day_title = "Knot Hash"
+
+SUFFIX = [17, 31, 73, 47, 23]
+
+
+def read_input(filename: str) -> List[str]:
+    with open(filename) as f:
+        content = [x.strip('\n') for x in f.readlines()]
+    return content
+
+
+def swap(numbers: List[int], i: int, j: int):
+    temp = numbers[i]
+    numbers[i] = numbers[j]
+    numbers[j] = temp
+
+
+def reverse(numbers: List[int], curr: int, inst: int):
+    end = curr + inst - 1
+    num_swaps = int((inst)/2)
+    for x in range(num_swaps):
+        i = (curr + x) % len(numbers)
+        j = (end - x) % len(numbers)
+        swap(numbers, i, j)
+
+
+def knot_hash(numbers: List[int], inst: List[int]) -> int:
+    curr = 0
+    skip_size = 0
+    for i in inst:
+        reverse(numbers, curr, i)
+        curr = curr + i + skip_size
+        skip_size += 1
+    return numbers[0] * numbers[1]
+
+
+def asciiCode(s: List[str]) -> List[str]:
+    b = []
+    for i in s:
+        b.append(ord(i))
+    return b
+
+
+def toHex(s: str) -> str:
+    return "%0.2x" % s
+
+
+def dense_knot_hash(numbers: List[int], inst: List[str]) -> str:
+    a = asciiCode(inst)
+    ass = a + SUFFIX
+    as64 = ass * 64
+    knot_hash(numbers, as64)
+
+    # XOR the 16th elements
+    dense_hash = list()
+    prev = 0
+    for i in range(16, len(numbers)+1, 16):
+        slice = numbers[prev:i]
+        acc = slice[0]
+        for j in range(1, len(slice)):
+            acc = acc ^ slice[j]
+        # things to do before iterating
+        dense_hash.append(acc)
+        prev = i
+
+    # convert dense hash to hex
+    result = ""
+    for i in dense_hash:
+        result = result + toHex(i)
+    return result
+
+
+def main():
+    print("Day", day_num, ":", day_title)
+    inputs = read_input('input.txt')[0]
+    ints = [int(x) for x in inputs.split(',')]
+    numbers = list(range(256))
+    print("Part 1: ", knot_hash(numbers, ints))
+    numbers = list(range(256))
+    print("Part 2: ", dense_knot_hash(numbers, inputs))
+
+
+if __name__ == '__main__':
+    main()
