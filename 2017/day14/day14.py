46f9716f6e7cf90c796963e4c9b8f8378d3ce505
Roland Kluge <roland.kluge@gmail.com>
2020-12-27 22:11:02 -0500

    2017 day14 redo
    Redid the style to match current AoC layout
    Also added type hinting
    
    Needs to work with imports and python packages, modules, submodules

--- 2017/day14/day14.py
+++ 2017/day14/day14.py
@@ -1,54 +1,72 @@
+from typing import List, Set, Tuple
+
+# FIXME: Need to figure out how to make this a proper python project, package, and module
 from day10 import dense_knot_hash
-import binascii
 
-print("Day 14")
+day_num = "14"
+day_title = "Disk Defragmentation"
 
-def dkh(p):
+PUZZLE = "amgozmfv"
+
+
+def dkh(p: List[str]) -> str:
     return dense_knot_hash(list(range(256)), p)
 
 
-def toBin(hex):
-    return "{0:8b}".format(int(hex,16))
+def toBin(hex: str) -> str:
+    return "{0:8b}".format(int(hex, 16))
 
 
-puzzle = "amgozmfv"
-count = 0
-grid = set()
-for i in range(128):
-    p = puzzle + "-" + str(i)
-    d = dkh(p)
-    b = toBin(d)
+def squares_used(puzzle: str, n: int) -> Tuple[int, Set[Tuple[int, int]]]:
+    count = 0
+    grid = set()
+    for i in range(n):
+        p = puzzle + "-" + str(i)
+        d = dkh(p)
+        b = toBin(d)
 
-    for x in range(len(b)):
-        print(x)
-        j = b[x]
-        if(j == "1"):
-            count += 1
-            grid.add((i,j))
+        for j in range(len(b)):
+            bit = b[j]
+            if(bit == "1"):
+                count += 1
+                grid.add((i, j))
+    return count, grid
 
-print("part1", count, len(grid))
-print("grid", grid)
-all_groups = list()
-for i in range(128):
-    for j in range(128):
-        p = (i,j)
-        if p in grid:
-            print("p is in grid", p)
-            # find the set within all_groups that p belongs too
-            s = None
-            for g in all_groups:
-                if p in g:
-                    s = g
-            # if not found, then create a new group and add it to all_groups
-            if s is None:
-                s = set()
-                all_groups.append(s)
-            
-            s.add(p)
-            # check the neighbors
-            n = [(i,j+1), (i,j-1), (i+1,j), (i-1,j)]
-            for k in n:
-                if k in grid:
-                    s.add(k)
-    
-print("group count", len(all_groups))
+
+def num_regions(grid: Set[Tuple[int, int]], n: int) -> List[Set[int]]:
+    all_regions: List[Set[int]] = []
+    for i in range(n):
+        for j in range(n):
+            p = (i, j)
+            if p in grid:
+                # find the set within all_groups that p belongs too
+                s = None
+                for g in all_regions:
+                    if p in g:
+                        s = g
+
+                # if not found create a new group add to all_groups
+                if s is None:
+                    s = set()
+                    s.add(p)
+                    all_regions.append(s)
+
+                # check the neighbors
+                n = [(i, j+1), (i, j-1), (i+1, j), (i-1, j)]
+                for k in n:
+                    if k in grid:
+                        s.add(k)
+
+    return all_regions
+
+
+def main():
+    print("Day", day_num, ":", day_title)
+    squares, grid = squares_used(PUZZLE, 128)
+    print("Part 1:", squares)
+    all_regions = num_regions(grid, 128)
+    print("Part 2:", len(all_regions))
+
+
+if __name__ == '__main__':
+    main()
