23c1670ec8c88ae3da9902eeb1f6c43b1c0ae805
Roland Kluge <roland.kluge@gmail.com>
2020-12-30 16:55:02 -0500

    2017 day15 complete

--- 2017/day15/day15.py
+++ 2017/day15/day15.py
@@ -6,39 +6,48 @@ day_title = "Dueling Generators"
 
 DIVISOR = 2147483647
 
+
 def read_input(filename: str) -> List[str]:
     with open(filename) as f:
         content = [x.strip('\n') for x in f.readlines()]
     return content
 
 
-def make_generator(value: int, factor: int) -> Iterator[int]:
+def make_generator(value: int, factor: int, mult: int) -> Iterator[int]:
     prev = value
     while True:
         value = (prev * factor) % DIVISOR
-        yield value
+        if value % mult == 0:
+            yield value
         prev = value
 
-def part1():
-    a = make_generator(289, 16807)
-    b = make_generator(629, 48271)
 
+def judge(a: Iterator[int], b: Iterator[int], num_pairs: int) -> int:
     bits = 0
     for i in range(16):
-        bits += 1<<i
+        bits += 1 << i
 
     count = 0
-    for _ in range(40_000_000):
+    for _ in range(num_pairs):
         a_value = next(a)
         b_value = next(b)
         if (a_value & bits) == (b_value & bits):
             count += 1
-    
-    print(f"Part 1:", count)
+    return count
+
+
+def part1():
+    a = make_generator(289, 16807, 1)
+    b = make_generator(629, 48271, 1)
+    count = judge(a, b, 40_000_000)
+    print(f"Part 1: {count}")
 
 
 def part2():
-    print("Part 2")
+    a = make_generator(289, 16807, 4)
+    b = make_generator(629, 48271, 8)
+    count = judge(a, b, 5_000_000)
+    print(f"Part 2: {count}")
 
 
 def main():
