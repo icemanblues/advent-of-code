28d505fd58ad42e21d44623f2899826d4ce47dbe
Roland Kluge <roland.kluge@gmail.com>
2020-12-31 14:33:17 -0500

    2017 day14 complete

--- 2017/day14/day14.py
+++ 2017/day14/day14.py
@@ -16,20 +16,22 @@ def toBin(hex: str) -> str:
     return "{0:8b}".format(int(hex, 16))
 
 
-def squares_used(puzzle: str, n: int) -> Tuple[int, Set[Tuple[int, int]]]:
-    count = 0
+def squares_used(puzzle: str, n: int) -> Set[Tuple[int, int]]:
     grid = set()
     for i in range(n):
         p = puzzle + "-" + str(i)
         d = dkh(p)
-        b = toBin(d)
 
-        for j in range(len(b)):
+        # toBin drops the leading zeroes
+        b = toBin(d)
+        while len(b) < n:
+            b = "0" + b
+
+        for j in range(n):
             bit = b[j]
             if(bit == "1"):
-                count += 1
                 grid.add((i, j))
-    return count, grid
+    return grid
 
 
 def num_regions(grid: Set[Tuple[int, int]], n: int) -> List[Set[Tuple[int, int]]]:
@@ -38,48 +40,40 @@ def num_regions(grid: Set[Tuple[int, int]], n: int) -> List[Set[Tuple[int, int]]
         for j in range(n):
             p = (i, j)
             if p in grid:
-                # find the set within all_groups that p belongs too
-                s: Set[Tuple[int, int]] = set()
-                for g in all_regions:
-                    if p in g:
-                        s = g
+                # check if it already exists in a set
+                exists = False
+                for s in all_regions:
+                    if p in s:
+                        exists = True
+                        break
+                if exists:
+                    continue
 
-                # if not found create a new group add to all_groups
-                if len(s) == 0:
-                    s.add(p)
-                    all_regions.append(s)
+                # not in an existing set, so start a new one and grow it
+                # do bfs to find all of the neighbors and add them to the set
+                queue: List[Tuple[int, int]] = [p]
+                visited: Set[Tuple[int, int]] = set()
+                while len(queue) != 0:
+                    q = queue.pop(0)
+                    if q in visited:
+                        continue
+                    visited.add(q)
 
-                # check the neighbors
-                neighbors: List[Tuple[int, int]] = [(i, j+1), (i, j-1), (i+1, j), (i-1, j)]
-                for k in neighbors:
-                    if k in grid:
-                        s.add(k)
-
+                    neighbors: List[Tuple[int, int]] = [(q[0], q[1]+1), (q[0], q[1]-1),
+                                                        (q[0]+1, q[1]), (q[0]-1, q[1])]
+                    for sq in neighbors:
+                        if sq in grid:
+                            queue.append(sq)
+                all_regions.append(visited)
     return all_regions
 
 
-def reduceSets(sets: List[Set[Tuple[int, int]]]) -> int:
-    removed: Set[int] = set()
-    for i in range(len(sets)-1):
-        for j in range(1, len(sets)):
-            if j in removed or i in removed:
-                continue
-            s1 = sets[i]
-            s2 = sets[j]
-            if len(s2.intersection(s1)) != 0:
-                s1 = s1.union(s2)
-                sets[i] = s1
-                removed.add(j)
-
-    return len(sets) - len(removed)
-
 def main():
     print("Day", day_num, ":", day_title)
-    squares, grid = squares_used(INPUT, 128)
-    print("Part 1:", squares)
+    grid = squares_used(INPUT, 128)
+    print("Part 1:", len(grid))
     all_regions = num_regions(grid, 128)
-    reduced_regions = reduceSets(all_regions)
-    print("Part 2:", reduced_regions)
+    print("Part 2:", len(all_regions))
 
 
 if __name__ == '__main__':
