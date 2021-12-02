5a24e762f7e20da5733fe129bff99882b52567cb
Roland Kluge <rolandkluge@yahoo.com>
2021-12-02 08:25:54 -0500

    2021 day02 complete

--- 2021/day02/day02.go
+++ 2021/day02/day02.go
@@ -2,25 +2,61 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/icemanblues/advent-of-code/pkg/util"
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "02"
+	dayTitle = "Dive!"
 )
 
 func part1() {
 	fmt.Println("Part 1")
 	input, _ := util.ReadInput("input.txt")
-	fmt.Println(input)
+
+	horizontal, depth := 0, 0
+	for _, step := range input {
+		splits := strings.Split(step, " ")
+		switch splits[0] {
+		case "forward":
+			horizontal += util.MustAtoi(splits[1])
+		case "down":
+			depth += util.MustAtoi(splits[1])
+		case "up":
+			depth -= util.MustAtoi(splits[1])
+		default:
+			fmt.Printf("unknown direction: %v\n", input)
+		}
+	}
+
+	answer := horizontal * depth
+	fmt.Println(answer)
 }
 
 func part2() {
 	fmt.Println("Part 2")
-	runes, _ := util.ReadRuneput("input.txt")
-	fmt.Println(runes)
+	input, _ := util.ReadInput("input.txt")
+
+	horizontal, depth, aim := 0, 0, 0
+	for _, step := range input {
+		splits := strings.Split(step, " ")
+		switch splits[0] {
+		case "forward":
+			horizontal += util.MustAtoi(splits[1])
+			depth += aim * util.MustAtoi(splits[1])
+		case "down":
+			aim += util.MustAtoi(splits[1])
+		case "up":
+			aim -= util.MustAtoi(splits[1])
+		default:
+			fmt.Printf("unknown direction: %v\n", input)
+		}
+	}
+
+	answer := horizontal * depth
+	fmt.Println(answer)
 }
 
 func main() {
