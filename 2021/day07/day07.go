817792e4f7efedf33a749d9aabf0c90593833fa9
Roland Kluge <rolandkluge@yahoo.com>
2021-12-07 19:45:10 -0500

    2021 day07 complete

--- 2021/day07/day07.go
+++ 2021/day07/day07.go
@@ -7,20 +7,60 @@ import (
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "07"
+	dayTitle = "The Treachery of Whales"
 )
 
+type DistFormula func(int, int) int
+
+func dist(a, b int) int {
+	return util.Abs(a - b)
+}
+
+func sumNum(a, b int) int {
+	a = util.Abs(a - b)
+	return (a * (a + 1)) / 2
+}
+
+func search(input []int, f DistFormula) (int, int) {
+	// establish our search boundaries (inclusive)
+	min, max := input[0], input[0]
+	for i := 1; i < len(input); i++ {
+		if input[i] < min {
+			min = input[i]
+		}
+		if input[i] > max {
+			max = input[i]
+		}
+	}
+
+	// find the lowest fuel via the distance function
+	minFuel := -1
+	minIndex := -1
+	for i := min; i <= max; i++ {
+		fuel := 0
+		for _, e := range input {
+			fuel += f(e, i)
+		}
+		if minFuel == -1 || minFuel > fuel {
+			minFuel = fuel
+			minIndex = i
+		}
+	}
+
+	return minFuel, minIndex
+}
+
 func part1() {
-	fmt.Println("Part 1")
-	input, _ := util.ReadInput("input.txt")
-	fmt.Println(input)
+	input, _ := util.ReadIntLine("input.txt", ",")
+	minFuel, minIndex := search(input, dist)
+	fmt.Printf("Part 1: fuel: %v index: %v\n", minFuel, minIndex)
 }
 
 func part2() {
-	fmt.Println("Part 2")
-	runes, _ := util.ReadRuneput("input.txt")
-	fmt.Println(runes)
+	input, _ := util.ReadIntLine("input.txt", ",")
+	minFuel, minIndex := search(input, sumNum)
+	fmt.Printf("Part 2: fuel: %v index: %v\n", minFuel, minIndex)
 }
 
 func main() {
