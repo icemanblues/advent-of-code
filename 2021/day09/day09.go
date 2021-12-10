25f1376e630b7335501d3bed9b1a5a86b8f6fa42
Roland Kluge <rolandkluge@yahoo.com>
2021-12-09 21:29:50 -0500

    2021 day09 complete

--- 2021/day09/day09.go
+++ 2021/day09/day09.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 
 	"github.com/icemanblues/advent-of-code/pkg/util"
 )
@@ -34,10 +35,51 @@ func Parse(filename string) ([][]int, error) {
 	return grid, nil
 }
 
-func part1() {
-	fmt.Println("Part 1")
+type Point struct{ x, y int }
+
+func expand(grid [][]int, p Point) int {
+	var queue []Point
+	queue = append(queue, p)
+	visited := make(map[Point]struct{})
+	size := 0
+
+	for len(queue) != 0 {
+		q := queue[0]
+		queue = queue[1:]
+
+		if _, ok := visited[q]; ok {
+			continue
+		}
+		if grid[q.y][q.x] == 9 {
+			continue
+		}
+
+		size++
+		visited[q] = struct{}{}
+
+		if q.x > 0 {
+			queue = append(queue, Point{q.x - 1, q.y})
+		}
+		if q.x < len(grid[q.y])-1 {
+			queue = append(queue, Point{q.x + 1, q.y})
+		}
+		if q.y > 0 {
+			queue = append(queue, Point{q.x, q.y - 1})
+		}
+		if q.y < len(grid)-1 {
+			queue = append(queue, Point{q.x, q.y + 1})
+		}
+	}
+
+	return size
+}
+
+func main() {
+	fmt.Printf("Day %v: %v\n", dayNum, dayTitle)
 	grid, _ := Parse("input.txt")
+
 	risk := 0
+	var lowPoints []Point
 	for y := 0; y < len(grid); y++ {
 		for x := 0; x < len(grid[y]); x++ {
 			top, left, right, bot := true, true, true, true
@@ -55,21 +97,19 @@ func part1() {
 			}
 
 			if top && bot && left && right {
-				//fmt.Printf("low point: x:%v y:%v height:%v\n", x, y, grid[y][x])
 				risk += grid[y][x] + 1
+				lowPoints = append(lowPoints, Point{x, y})
 			}
 		}
 	}
-
 	fmt.Printf("Part 1: %v\n", risk)
-}
 
-func part2() {
-	fmt.Println("Part 2")
-}
-
-func main() {
-	fmt.Printf("Day %v: %v\n", dayNum, dayTitle)
-	part1()
-	part2()
+	sizes := make([]int, 0, len(lowPoints))
+	for _, point := range lowPoints {
+		sizes = append(sizes, expand(grid, point))
+	}
+	sort.Ints(sizes)
+	l := len(sizes)
+	answer := sizes[l-1] * sizes[l-2] * sizes[l-3]
+	fmt.Printf("Part 2: %v\n", answer)
 }
