63302a12b47c93e36957f6ccba6e247633229e17
Roland Kluge <rolandkluge@yahoo.com>
2021-12-10 20:50:56 -0500

    2021 day10 complete

--- 2021/day10/day10.go
+++ 2021/day10/day10.go
@@ -2,29 +2,81 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/icemanblues/advent-of-code/pkg/util"
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "10"
+	dayTitle = "Syntax Scoring"
 )
 
-func part1() {
-	fmt.Println("Part 1")
-	input, _ := util.ReadInput("input.txt")
-	fmt.Println(input)
+var scoring map[rune]int = map[rune]int{
+	')': 3,
+	']': 57,
+	'}': 1197,
+	'>': 25137,
 }
 
-func part2() {
-	fmt.Println("Part 2")
-	runes, _ := util.ReadRuneput("input.txt")
-	fmt.Println(runes)
+var autocomplete map[rune]int = map[rune]int{
+	'(': 1,
+	'[': 2,
+	'{': 3,
+	'<': 4,
+}
+
+var closeToOpen map[rune]rune = map[rune]rune{
+	')': '(',
+	']': '[',
+	'}': '{',
+	'>': '<',
+}
+
+func score(chunk []rune) (int, int) {
+	var stack []rune
+	for _, r := range chunk {
+		if _, ok := closeToOpen[r]; !ok { // opening bracket
+			stack = append(stack, r)
+		} else { // closing bracket
+			if len(stack) == 0 {
+				return scoring[r], 0
+			}
+
+			open := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if open != closeToOpen[r] {
+				return scoring[r], 0
+			}
+		}
+	}
+
+	if len(stack) == 0 {
+		return 0, 0
+	}
+
+	// its incomplete, so we score it differently (part2)
+	autoscore := 0
+	for i := len(stack) - 1; i >= 0; i-- {
+		autoscore = (5 * autoscore) + autocomplete[stack[i]]
+	}
+	return 0, autoscore
 }
 
 func main() {
 	fmt.Printf("Day %v: %v\n", dayNum, dayTitle)
-	part1()
-	part2()
+	runes, _ := util.ReadRuneput("input.txt")
+	sum := 0
+	var autocomplete []int
+	for _, line := range runes {
+		s, a := score(line)
+		sum += s
+		if a != 0 {
+			autocomplete = append(autocomplete, a)
+		}
+	}
+	fmt.Printf("Part 1: %v\n", sum)
+
+	sort.Ints(autocomplete)
+	fmt.Printf("Part 2: %v\n", autocomplete[len(autocomplete)/2])
 }
