4e7f105f32ab0d82aa62c3a36f0e6841b91b4aff
Roland Kluge <rolandkluge@yahoo.com>
2021-12-12 23:19:40 -0500

    2021 day 11 complete

--- 2021/day11/day11.go
+++ 2021/day11/day11.go
@@ -7,20 +7,108 @@ import (
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "11"
+	dayTitle = "Dumbo Octopus"
 )
 
+func Parse(filename string) (grid [][]int) {
+	input, _ := util.ReadRuneput(filename)
+	for _, runes := range input {
+		row := make([]int, 0, len(runes))
+		for _, r := range runes {
+			row = append(row, util.MustAtoi(string(r)))
+		}
+		grid = append(grid, row)
+	}
+	return
+
+}
+
+type Point struct{ x, y int }
+
+func step(grid [][]int) int {
+	// increase all by 1
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[y]); x++ {
+			grid[y][x]++
+		}
+	}
+
+	// search for flashes
+	var flashing []Point
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[y]); x++ {
+			p := Point{x, y}
+			if grid[y][x] > 9 {
+				flashing = append(flashing, p)
+			}
+		}
+	}
+
+	// visit the ones that are flashing.
+	visited := make(map[Point]struct{})
+	for len(flashing) != 0 {
+		p := flashing[0]
+		flashing = flashing[1:]
+
+		if _, ok := visited[p]; ok {
+			continue
+		}
+
+		visited[p] = struct{}{}
+
+		// find adj
+		for yi := -1; yi <= 1; yi++ {
+			for xi := -1; xi <= 1; xi++ {
+				if xi == 0 && yi == 0 {
+					continue
+				}
+				yy := p.y + yi
+				if yy < 0 || yy >= len(grid) {
+					continue
+				}
+				xx := p.x + xi
+				if xx < 0 || xx >= len(grid[yy]) {
+					continue
+				}
+
+				grid[yy][xx]++
+				if grid[yy][xx] > 9 {
+					flashing = append(flashing, Point{xx, yy})
+				}
+			}
+		}
+	}
+
+	// energy reset to 0
+	for y := 0; y < len(grid); y++ {
+		for x := 0; x < len(grid[y]); x++ {
+			if grid[y][x] > 9 {
+				grid[y][x] = 0
+			}
+		}
+	}
+
+	return len(visited)
+}
+
 func part1() {
-	fmt.Println("Part 1")
-	input, _ := util.ReadInput("input.txt")
-	fmt.Println(input)
+	grid := Parse("input.txt")
+	score := 0
+	for steps := 0; steps < 100; steps++ {
+		score += step(grid)
+	}
+	fmt.Printf("Part 1: %v\n", score)
 }
 
 func part2() {
-	fmt.Println("Part 2")
-	runes, _ := util.ReadRuneput("input.txt")
-	fmt.Println(runes)
+	grid := Parse("input.txt")
+	target := len(grid) * len(grid[0])
+	score, steps := 0, 0
+	for ; score != target; steps++ {
+		score = step(grid)
+	}
+	fmt.Printf("Part 2: %v\n", steps)
 }
 
 func main() {
