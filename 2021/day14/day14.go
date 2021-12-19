a6979bf415173c47dd114f7d9a29eb92453ee66c
Roland Kluge <rolandkluge@yahoo.com>
2021-12-18 21:10:41 -0500

    2021 14 complete

--- 2021/day14/day14.go
+++ 2021/day14/day14.go
@@ -47,13 +47,9 @@ func step(start string, rules []Rule) string {
 	return builder.String()
 }
 
-func score(polymer string) int {
-	counts := make(map[rune]int)
-	for _, r := range polymer {
-		counts[r]++
-	}
-	min, max := -1, -1
-	for _, v := range counts {
+func score(polymer runecount) int64 {
+	var min, max int64 = -1, -1
+	for _, v := range polymer {
 		if min == -1 {
 			min = v
 		}
@@ -77,10 +73,80 @@ func part1() {
 	for i := 0; i < 10; i++ {
 		start = step(start, rules)
 	}
-	fmt.Printf("Part1: %v\n", score(start))
+	counts := make(runecount)
+	for _, r := range start {
+		counts[r]++
+	}
+
+	fmt.Printf("Part 1: %v\n", score(counts))
 }
 
-func part2() {}
+type runecount map[rune]int64
+
+func elements(polymer string, counts runecount) runecount {
+	for _, r := range polymer {
+		counts[r]++
+	}
+	return counts
+}
+
+type Reaction struct {
+	in     string
+	out    []string
+	insert rune
+}
+
+func rulesToReaction(rule Rule) Reaction {
+	return Reaction{
+		rule.reagents,
+		[]string{
+			rule.reagents[:1] + rule.results,
+			rule.results + rule.reagents[1:],
+		},
+		[]rune(rule.results)[0],
+	}
+}
+
+func part2() {
+	start, rules := Parse("input.txt")
+
+	elementCounts := make(runecount)
+	elements(start, elementCounts)
+
+	reactions := make([]Reaction, 0, len(rules))
+	for _, rule := range rules {
+		reactions = append(reactions, rulesToReaction(rule))
+	}
+
+	pairCounts := make(map[string]int64)
+	for i := 0; i < len(start)-1; i++ {
+		pairCounts[start[i:i+2]]++
+	}
+
+	// run the number of steps
+	for i := 0; i < 40; i++ {
+		newPairs := make(map[string]int64)
+		for pair, count := range pairCounts {
+			// find the corresponding reaction // this should be a map lookup. ugh!
+			var reaction Reaction
+			for _, re := range reactions {
+				if re.in == pair {
+					reaction = re
+				}
+			}
+
+			// add the new pairs and rune
+			for _, p := range reaction.out {
+				newPairs[p] += count
+			}
+			elementCounts[reaction.insert] += count
+		}
+		//iterate
+		pairCounts = newPairs
+	}
+
+	fmt.Printf("Part 2: %v\n", score(elementCounts))
+}
 
 func main() {
 	fmt.Printf("Day %v: %v\n", dayNum, dayTitle)
