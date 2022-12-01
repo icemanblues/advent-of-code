d7c112992f5ad80c511f6db2bbc73e71d83b4532
Roland Kluge <rolandkluge@yahoo.com>
2022-12-01 13:10:46 -0500

    2022 day01 completed

--- 2022/day01/day01.go
+++ 2022/day01/day01.go
@@ -2,29 +2,54 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strconv"
 
 	"github.com/icemanblues/advent-of-code/pkg/util"
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "01"
+	dayTitle = "Calorie Counting"
 )
 
-func part1() {
-	fmt.Println("Part 1")
-	input, _ := util.ReadInput("input.txt")
-	fmt.Println(input)
+type Elf struct {
+	Calories []int
+	Total    int
 }
 
-func part2() {
-	fmt.Println("Part 2")
-	runes, _ := util.ReadRuneput("input.txt")
-	fmt.Println(runes)
+func NewElf(calories []int) Elf {
+	sum := 0
+	for _, c := range calories {
+		sum += c
+	}
+	return Elf{calories, sum}
 }
 
 func main() {
 	fmt.Printf("Day %v: %v\n", dayNum, dayTitle)
-	part1()
-	part2()
+	input, _ := util.ReadInput("input.txt")
+	var elves []Elf
+	var calories []int
+	for _, line := range input {
+		if line == "" {
+			elves = append(elves, NewElf(calories))
+			calories = nil
+			continue
+		}
+		a, _ := strconv.Atoi(line)
+		calories = append(calories, a)
+	}
+	elves = append(elves, NewElf(calories))
+	calories = nil
+
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i].Total < elves[j].Total
+	})
+	l := len(elves) - 1
+	max := elves[l].Total
+	maxThree := elves[l].Total + elves[l-1].Total + elves[l-2].Total
+
+	fmt.Printf("Part 1: %v\n", max)
+	fmt.Printf("Part 2: %v\n", maxThree)
 }
