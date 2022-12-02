a3d67bb2ed2abb2d750333edb8936c96685d16ae
Roland Kluge <rolandkluge@yahoo.com>
2022-12-02 16:57:27 -0500

    2022 day02 complete

--- 2022/day02/day02.go
+++ 2022/day02/day02.go
@@ -7,24 +7,104 @@ import (
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "02"
+	dayTitle = "Rock Paper Scissors"
 )
 
-func part1() {
-	fmt.Println("Part 1")
-	input, _ := util.ReadInput("input.txt")
-	fmt.Println(input)
+type RPS int
+
+const (
+	Rock    RPS = 1
+	Paper   RPS = 2
+	Scissor RPS = 3
+)
+
+type Inst struct {
+	ElfCode, HumanCode rune
+	ElfRps, HumanRps   RPS
 }
 
-func part2() {
-	fmt.Println("Part 2")
-	runes, _ := util.ReadRuneput("input.txt")
-	fmt.Println(runes)
+var ElfMap map[rune]RPS = map[rune]RPS{
+	'A': Rock,
+	'B': Paper,
+	'C': Scissor,
+}
+
+var HumanMap map[rune]RPS = map[rune]RPS{
+	'X': Rock,
+	'Y': Paper,
+	'Z': Scissor,
+}
+
+func Play(insts []Inst) int {
+	score := 0
+	for _, inst := range insts {
+		score += PlayRPS(inst.ElfRps, inst.HumanRps)
+	}
+	return score
+}
+
+func PlayRPS(elf, human RPS) int {
+	if elf == human {
+		return 3 + int(human)
+	}
+
+	if elf == Rock && human == Scissor || elf == Scissor && human == Paper ||
+		elf == Paper && human == Rock { // elf wins
+		return 0 + int(human)
+	} else { // human wins
+		return 6 + int(human)
+	}
+}
+
+func humanPlay(elf RPS, outcome rune) RPS {
+	switch outcome {
+	case 'X': // lose
+		switch elf {
+		case Rock:
+			return Scissor
+		case Paper:
+			return Rock
+		case Scissor:
+			return Paper
+		}
+	case 'Y': // draw
+		switch elf {
+		case Rock:
+			return Rock
+		case Paper:
+			return Paper
+		case Scissor:
+			return Scissor
+		}
+	case 'Z': // win
+		switch elf {
+		case Rock:
+			return Paper
+		case Paper:
+			return Scissor
+		case Scissor:
+			return Rock
+		}
+	}
+
+	panic("Impossible!")
 }
 
 func main() {
 	fmt.Printf("Day %v: %v\n", dayNum, dayTitle)
-	part1()
-	part2()
+
+	input, _ := util.ReadRuneput("input.txt")
+	insts := make([]Inst, 0, len(input))
+	for _, runes := range input {
+		insts = append(insts, Inst{runes[0], runes[2], ElfMap[runes[0]], HumanMap[runes[2]]})
+	}
+	fmt.Printf("Part 1: %v\n", Play(insts))
+
+	cheats := make([]Inst, 0, len(insts))
+	for _, inst := range insts {
+		move := humanPlay(inst.ElfRps, inst.HumanCode)
+		cheats = append(cheats, Inst{inst.ElfCode, inst.HumanCode, inst.ElfRps, move})
+	}
+	fmt.Printf("Part 2: %v\n", Play(cheats))
 }
