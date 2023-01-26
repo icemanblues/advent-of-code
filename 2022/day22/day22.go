75c60237c0a9d22d73d9f4a1c566619a6b397df7
Roland Kluge <rolandkluge@yahoo.com>
2023-01-26 12:09:15 -0500

    new branch to kick off day 22

--- 2022/day22/day22.go
+++ 2022/day22/day22.go
@@ -7,21 +7,106 @@ import (
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "22"
+	dayTitle = "Monkey Map"
 )
 
-func part1() {
-	fmt.Println("Part 1")
-	input, _ := util.ReadInput("input.txt")
-	fmt.Println(input)
+type Grid map[util.Point]rune
+
+type Direction rune
+
+const (
+	U Direction = 'U'
+	R Direction = 'R'
+	D Direction = 'D'
+	L Direction = 'L'
+)
+
+var Movement map[Direction]util.Point = map[Direction]util.Point{
+	U: util.NewPoint(0, -1),
+	R: util.NewPoint(1, 0),
+	D: util.NewPoint(0, 1),
+	L: util.NewPoint(-1, 0),
 }
 
-func part2() {
-	fmt.Println("Part 2")
-	runes, _ := util.ReadRuneput("input.txt")
-	fmt.Println(runes)
+func Turn(d Direction, t rune) Direction {
+	switch t {
+	case 'R':
+		switch d {
+		case U:
+			return R
+		case R:
+			return D
+		case D:
+			return L
+		case L:
+			return U
+		default:
+			fmt.Printf("Unknown direction for Turn: %c\n", d)
+			return d
+		}
+	case 'L':
+		switch d {
+		case U:
+			return L
+		case R:
+			return U
+		case D:
+			return R
+		case L:
+			return D
+		default:
+			fmt.Printf("Unknown direction for Turn: %c\n", d)
+			return d
+		}
+	default:
+		fmt.Printf("Unknown turn type for Turn: %c\n", t)
+		return d
+	}
 }
+
+func Move(p util.Point, d Direction) util.Point {
+	offset, ok := Movement[d]
+	if !ok {
+		fmt.Printf("Unknown direction for Move: %c\n", d)
+		return p
+	}
+	return util.NewPoint(p.X+offset.X, p.Y+offset.Y)
+}
+
+func Parse(filename string) int {
+	parseMap := true
+	grid := make(Grid)
+	input, _ := util.ReadInput("input.txt")
+	for y, line := range input {
+		if line == "" {
+			parseMap = false
+			continue
+		}
+
+		if parseMap {
+			// add it to the grid
+			for x, r := range line {
+				if r == '.' || r == '#' {
+					p := util.NewPoint(x, y)
+					grid[p] = r
+				}
+				// TODO: probably want the max X and max Y for boundary limits
+			}
+		} else {
+			// parse the movement instructions
+		}
+	}
+
+	return len(input)
+}
+
+func part1() {
+	n := Parse("input.txt")
+	fmt.Printf("Part 1: %v\n", n)
+}
+
+func part2() {}
 
 func main() {
 	fmt.Printf("Day %v: %v\n", dayNum, dayTitle)
