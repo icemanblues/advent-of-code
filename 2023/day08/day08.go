691a18af660379f243a136dc04c6eb4de42d7889
Roland Kluge <rolandkluge@yahoo.com>
2023-12-08 10:27:34 -0500

    2023 day08 complete

--- 2023/day08/day08.go
+++ 2023/day08/day08.go
@@ -2,21 +2,90 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/icemanblues/advent-of-code/pkg/util"
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "08"
+	dayTitle = "Haunted Wasteland"
 )
 
+var START, END string = "AAA", "ZZZ"
+
+type Node struct {
+	Name, Left, Right string
+}
+
+type Directions struct {
+	path  []rune
+	steps int
+}
+
+func (d *Directions) Next() rune {
+	r := d.path[d.steps%len(d.path)]
+	d.steps++
+	return r
+}
+
+func ParseMap(filename string) (Directions, map[string]Node) {
+	input, _ := util.ReadInput(filename)
+	d := Directions{[]rune(input[0]), 0}
+	nodeList := make(map[string]Node)
+	for _, line := range input[2:] {
+		nodeAdjList := strings.Split(line, " = ")
+		leftRight := strings.Split(nodeAdjList[1], ", ")
+		nodeList[nodeAdjList[0]] = Node{
+			Name:  nodeAdjList[0],
+			Left:  leftRight[0][1:],
+			Right: leftRight[1][:len(leftRight[1])-1],
+		}
+	}
+	return d, nodeList
+}
+
+func StepCount(d Directions, nodeMap map[string]Node, start string, ends map[string]struct{}) int {
+	for start != END {
+		next := d.Next()
+		switch next {
+		case 'L':
+			start = nodeMap[start].Left
+		case 'R':
+			start = nodeMap[start].Right
+		default:
+			fmt.Printf("ERROR: Unknown next step: %v\n", next)
+			break
+		}
+		if _, ok := ends[start]; ok {
+			break
+		}
+	}
+	return d.steps
+}
+
 func part1() {
-	input, _ := util.ReadInput("input.txt")
-	fmt.Printf("Part 1: %v\n", len(input))
+	d, nodeMap := ParseMap("input.txt")
+	fmt.Printf("Part 1: %v\n", StepCount(d, nodeMap, START, map[string]struct{}{END: {}}))
 }
 
 func part2() {
+	d, nodeMap := ParseMap("input.txt")
+	var starts []string
+	ends := make(map[string]struct{})
+	for name := range nodeMap {
+		if strings.HasSuffix(name, "A") {
+			starts = append(starts, name)
+		}
+		if strings.HasSuffix(name, "Z") {
+			ends[name] = struct{}{}
+		}
+	}
+	steps := make([]int, len(starts), len(starts))
+	for i, curr := range starts {
+		steps[i] = StepCount(Directions{d.path, 0}, nodeMap, curr, ends)
+	}
+	fmt.Printf("Part 2: %v\n", util.LCM(steps[0], steps[1], steps[2:]...))
 }
 
 func main() {
--- pkg/util/math.go
+++ pkg/util/math.go
@@ -26,3 +26,26 @@ func Abs(a int) int {
 func IntPow(a, b int) int {
 	return int(math.Pow(float64(a), float64(b)))
 }
+
+// greatest common divisor (GCD) via Euclidean algorithm
+// https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/
+func GCD(a, b int) int {
+	for b != 0 {
+		t := b
+		b = a % b
+		a = t
+	}
+	return a
+}
+
+// find Least Common Multiple (LCM) via GCD
+// https://siongui.github.io/2017/06/03/go-find-lcm-by-gcd/
+func LCM(a, b int, integers ...int) int {
+	result := a * b / GCD(a, b)
+
+	for i := 0; i < len(integers); i++ {
+		result = LCM(result, integers[i])
+	}
+
+	return result
+}
