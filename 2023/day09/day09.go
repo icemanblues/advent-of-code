be78b8613ec121e93ff6e364d7040a6c039138cb
Roland Kluge <rolandkluge@yahoo.com>
2023-12-11 10:25:15 -0500

    2023 day09 complete

--- 2023/day09/day09.go
+++ 2023/day09/day09.go
@@ -7,20 +7,44 @@ import (
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "09"
+	dayTitle = "Mirage Maintenance"
 )
 
-func part1() {
-	input, _ := util.ReadInput("input.txt")
-	fmt.Printf("Part 1: %v\n", len(input))
+func extrapolate(seq []int) int {
+	deriv := make([]int, len(seq)-1, len(seq)-1)
+	isAllZero := true
+	for i := 1; i < len(seq); i++ {
+		v := seq[i] - seq[i-1]
+		deriv[i-1] = v
+		isAllZero = isAllZero && v == 0
+	}
+
+	if isAllZero {
+		return seq[len(seq)-1]
+	}
+
+	d := extrapolate(deriv)
+	return seq[len(seq)-1] + d
 }
 
-func part2() {
+func Reverse(a []int) []int {
+	b := make([]int, 0, len(a))
+	for i := len(a) - 1; i >= 0; i-- {
+		b = append(b, a[i])
+	}
+	return b
 }
 
 func main() {
 	fmt.Printf("Day %v: %v\n", dayNum, dayTitle)
-	part1()
-	part2()
+	input, _ := util.ReadIntGrid("input.txt", " ")
+	sum, revSum := 0, 0
+	for _, seq := range input {
+		sum += extrapolate(seq)
+		rev := Reverse(seq)
+		revSum += extrapolate(rev)
+	}
+	fmt.Printf("Part 1: %v\n", sum)
+	fmt.Printf("Part 2: %v\n", revSum)
 }
