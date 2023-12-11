e160a54144f91cbcc178832dfa3a75e910bcfbdb
Roland Kluge <rolandkluge@yahoo.com>
2023-12-11 15:09:31 -0500

    2023 day11 complete

--- 2023/day11/day11.go
+++ 2023/day11/day11.go
@@ -7,16 +7,75 @@ import (
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "11"
+	dayTitle = "Cosmic Expansion"
 )
 
+func Expand(galaxies map[util.Point]struct{}, expandSize int) map[util.Point]struct{} {
+	rows := make(map[int]struct{})
+	cols := make(map[int]struct{})
+	maxX, maxY := 0, 0
+	for p := range galaxies {
+		rows[p.X] = struct{}{}
+		cols[p.Y] = struct{}{}
+		if maxX < p.X {
+			maxX = p.X
+		}
+		if maxY < p.Y {
+			maxY = p.Y
+		}
+	}
+
+	xCount := 0
+	xVoids := make([]int, 0, maxX+1)
+	for x := 0; x <= maxX; x++ {
+		xVoids = append(xVoids, xCount)
+		if _, ok := rows[x]; !ok {
+			xCount += expandSize - 1
+		}
+	}
+	yCount := 0
+	yVoids := make([]int, 0, maxY+1)
+	for y := 0; y <= maxY; y++ {
+		yVoids = append(yVoids, yCount)
+		if _, ok := cols[y]; !ok {
+			yCount += expandSize - 1
+		}
+	}
+
+	expanded := make(map[util.Point]struct{})
+	for g := range galaxies {
+		dx := xVoids[g.X]
+		dy := yVoids[g.Y]
+		expanded[util.Point{X: g.X + dx, Y: g.Y + dy}] = struct{}{}
+	}
+	return expanded
+}
+
+func shortPathSum(galaxy map[util.Point]struct{}) int {
+	sum := 0
+	points := make([]util.Point, 0, len(galaxy))
+	for g := range galaxy {
+		points = append(points, g)
+	}
+	for i := 0; i < len(points)-1; i++ {
+		for j := i + 1; j < len(points); j++ {
+			sum += util.ManhattanDist(points[i], points[j])
+		}
+	}
+	return sum
+}
+
 func part1() {
-	input, _ := util.ReadInput("input.txt")
-	fmt.Printf("Part 1: %v\n", len(input))
+	galaxies, _ := util.ReadSparseGrid("input.txt", '#')
+	expanded := Expand(galaxies, 2)
+	fmt.Printf("Part 1: %v\n", shortPathSum(expanded))
 }
 
 func part2() {
+	galaxies, _ := util.ReadSparseGrid("input.txt", '#')
+	expanded := Expand(galaxies, 1000000)
+	fmt.Printf("Part 2: %v\n", shortPathSum(expanded))
 }
 
 func main() {
