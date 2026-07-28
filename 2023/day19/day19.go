39d0a3c61755fc9891ac2f4d256d711631014811
Roland Kluge <rolandkluge@yahoo.com>
2026-07-28 18:32:51 -0400

    2023. day19 day25. Unsure of the status of these. don't want to lose them. Might be incomplete or not up to par

--- 2023/day19/day19.go
+++ 2023/day19/day19.go
@@ -2,21 +2,144 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/icemanblues/advent-of-code/pkg/util"
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "19"
+	dayTitle = "Aplenty"
 )
 
+type Tool struct {
+	x, m, a, s int
+}
+
+func (t Tool) rating() int {
+	return t.x + t.m + t.a + t.s
+}
+
+type Inst struct {
+	field string
+	op    string
+	value int
+	next  string
+}
+
+func (inst Inst) Test(t Tool) bool {
+	tv := t.x
+	switch inst.field {
+	case "x":
+		tv = t.x
+	case "m":
+		tv = t.m
+	case "a":
+		tv = t.a
+	case "s":
+		tv = t.s
+	}
+
+	if inst.op == ">" {
+		return tv > inst.value
+	}
+	return tv < inst.value
+
+}
+
+type Workflow struct {
+	name  string
+	insts []Inst
+}
+
+func (w Workflow) Apply(t Tool) string {
+	for _, inst := range w.insts {
+		if inst.op == "go" || inst.Test(t) {
+			return inst.next
+		}
+	}
+	return w.insts[len(w.insts)-1].next
+}
+
+func parse(filename string) (map[string]Workflow, []Tool) {
+	workflows := make(map[string]Workflow)
+	tools := make([]Tool, 0)
+	input, _ := util.ReadInput(filename)
+	isWorkflow := true
+	for _, line := range input {
+		if line == "" {
+			isWorkflow = false
+		} else if isWorkflow {
+			s := strings.Split(line, "{")
+			name := s[0]
+			flowsline := s[1][:len(s[1])-1]
+			flows := strings.Split(flowsline, ",")
+			insts := make([]Inst, 0, len(flows))
+			for _, flow := range flows {
+				fline := strings.Split(flow, ":")
+				if len(fline) == 1 { // just go to this location
+					insts = append(insts, Inst{"go", "go", 0, fline[0]})
+					continue
+				}
+				field := fline[0][:1]
+				op := fline[0][1:2]
+				value := util.MustAtoi(fline[0][2:])
+				nextName := fline[1]
+				insts = append(insts, Inst{field, op, value, nextName})
+			}
+			workflows[name] = Workflow{name, insts}
+		} else {
+			toolLine := line[1 : len(line)-1]
+			parts := strings.Split(toolLine, ",")
+			values := make([]int, 0, 4)
+			for _, p := range parts {
+				values = append(values, util.MustAtoi(strings.Split(p, "=")[1]))
+			}
+			tools = append(tools, Tool{values[0], values[1], values[2], values[3]})
+		}
+	}
+	return workflows, tools
+}
+
+func isAccepted(workflows map[string]Workflow, t Tool) bool {
+	name := "in"
+	for name != "A" && name != "R" {
+		name = workflows[name].Apply(t)
+	}
+	return name == "A"
+}
+
 func part1() {
-	input, _ := util.ReadInput("input.txt")
-	fmt.Printf("Part 1: %v\n", len(input))
+	workflows, tools := parse("input.txt")
+	var accepted []Tool
+	for _, t := range tools {
+		if isAccepted(workflows, t) {
+			accepted = append(accepted, t)
+		}
+	}
+	sum := 0
+	for _, t := range accepted {
+		sum += t.rating()
+	}
+	fmt.Printf("Part 1: %v\n", sum)
 }
 
 func part2() {
+	workflows, _ := parse("input.txt")
+	count := 0
+	for x := 1; x <= 4000; x++ {
+		for m := 1; m <= 4000; m++ {
+			for a := 1; a <= 4000; a++ {
+				for s := 1; s <= 4000; s++ {
+					t := Tool{x, m, a, s}
+					if isAccepted(workflows, t) {
+						count++
+					}
+				}
+			}
+		}
+	}
+	fmt.Printf("Part 2: %v\n", count)
 }
 
 func main() {
--- 2023/day25/day25.go
+++ 2023/day25/day25.go
@@ -2,25 +2,181 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 
 	"github.com/icemanblues/advent-of-code/pkg/util"
 )
 
 const (
-	dayNum   = "XX"
-	dayTitle = "Title"
+	dayNum   = "25"
+	dayTitle = "Snowverload"
 )
 
-func part1() {
-	input, _ := util.ReadInput("input.txt")
-	fmt.Printf("Part 1: %v\n", len(input))
+func contains(wires [][2]string, wire [2]string) bool {
+	for _, w := range wires {
+		if wire == w {
+			return true
+		}
+	}
+	return false
 }
 
-func part2() {
+func mapClone(m map[string][]string, cut [][2]string) map[string][]string {
+	clone := make(map[string][]string)
+	for k, v := range m {
+		for _, vv := range v {
+			c := [2]string{k, vv}
+			if !contains(cut, c) {
+				clone[k] = append(clone[k], vv)
+			}
+		}
+	}
+	return clone
+}
+
+func groupCount(m map[string][]string) []map[string]struct{} {
+	sets := make([]map[string]struct{}, 0)
+
+	for k, v := range m {
+		// get the existing set (if none, create it)
+		set := make(map[string]struct{})
+		found := false
+		for _, s := range sets {
+			if _, ok := s[k]; ok {
+				set = s
+				found = true
+			}
+		}
+		if !found {
+			sets = append(sets, set)
+		}
+		set[k] = struct{}{}
+		for _, vv := range v {
+			set[vv] = struct{}{}
+		}
+	}
+	return sets
+}
+
+func makeSets(m map[string][]string) []map[string]struct{} {
+	sets := make([]map[string]struct{}, 0)
+outer:
+	for k := range m {
+		if len(sets) > 2 { // specific to our problem space
+			return sets
+		}
+
+		// if k exists in a set. If it does, continue
+		for _, set := range sets {
+			if _, ok := set[k]; ok {
+				continue outer
+			}
+		}
+
+		// not in a set, so create one and populate it
+		set := make(map[string]struct{})
+		queue := []string{k}
+		for len(queue) != 0 {
+			curr := queue[0]
+			queue = queue[1:]
+
+			if _, ok := set[curr]; ok { // we've already visited
+				continue
+			}
+
+			set[curr] = struct{}{}
+			for _, next := range m[curr] {
+				queue = append(queue, next)
+			}
+		}
+		sets = append(sets, set)
+	}
+	return sets
+}
+
+func temp(adjList map[string][]string) {
+	try := mapClone(adjList, [][2]string{
+		{"hfx", "pzl"}, {"pzl", "hfx"},
+		{"bvb", "cmg"}, {"cmg", "bvb"},
+		{"nvd", "jqt"}, {"jqt", "nvd"},
+	})
+	//gg := groupCount(try)
+	gg := makeSets(try)
+	prod := 1
+	for i, e := range gg {
+		fmt.Printf("Set %v: %v\n", i, e)
+		prod *= len(e)
+	}
+	fmt.Printf("Part 1: %v\n", prod)
+	return
+
 }
 
 func main() {
 	fmt.Printf("Day %v: %v\n", dayNum, dayTitle)
-	part1()
-	part2()
+	input, _ := util.ReadInput("input.txt")
+	adjList := make(map[string][]string)
+	for _, line := range input {
+		split := strings.Split(line, ": ")
+		comps := strings.Fields(split[1])
+		for _, c := range comps {
+			adjList[split[0]] = append(adjList[split[0]], c)
+			adjList[c] = append(adjList[c], split[0])
+		}
+	}
+
+	// generate all possible wires that we can cut
+	wires := make([][2]string, 0)
+	for k, v := range adjList {
+		for _, w := range v {
+			wires = append(wires, [2]string{k, w})
+		}
+	}
+	fmt.Printf("number of wires: %v\n", len(wires))
+	sort.Slice(wires, func(i, j int) bool {
+		iLen := len(adjList[wires[i][0]]) + len(adjList[wires[i][1]])
+		jLen := len(adjList[wires[j][0]]) + len(adjList[wires[j][1]])
+		return iLen > jLen
+	})
+
+	// search for the 3 to cut
+	var g []map[string]struct{}
+search:
+	for i := 0; i < len(wires)-2; i++ {
+		for j := i + 1; j < len(wires)-1; j++ {
+			for k := j + 1; k < len(wires); k++ {
+				cut := [][2]string{
+					wires[i], {wires[i][1], wires[i][0]},
+					wires[j], {wires[j][1], wires[j][0]},
+					wires[k], {wires[k][1], wires[k][0]},
+				}
+
+				// check that the wires to be cut are valid (no dupes)
+				checker := make(map[[2]string]struct{})
+				for _, c := range cut {
+					checker[c] = struct{}{}
+				}
+				if len(checker) != 6 {
+					continue
+				}
+
+				// copy the map with removed wires
+				clone := mapClone(adjList, cut)
+
+				// count the number of connected groups
+				//g = groupCount(clone)
+				g = makeSets(clone)
+				if len(g) == 2 {
+					break search
+				}
+			}
+		}
+	}
+
+	// count it up
+	fmt.Println(g[0])
+	fmt.Println(g[1])
+	sum := len(g[0]) * len(g[1])
+	fmt.Printf("Part 1: %v\n", sum)
 }
